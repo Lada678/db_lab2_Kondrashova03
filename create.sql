f5910d92fdafb319b27095b68c9259e6ab3d0738
LadaKondrashova <55762041+Lada678@users.noreply.github.com>
2023-01-25 06:42:22 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,30 @@
+CREATE TABLE accident (
+	accident_id    serial       NOT NULL,
+	accident_date  date         NOT NULL,
+  country_id     int          NOT NULL,
+  description_id varchar(60)  NOT NULL,
+  accident_death int          
+  
+);
+
+CREATE TABLE country (
+	country_id     serial         NOT NULL,
+	country_name   varchar(30)    NOT NULL,
+  country_state  varchar(30)    
+ 
+);
+
+CREATE TABLE description (
+	description_id     serial         NOT NULL,
+	description_name   varchar(30)    NOT NULL
+  
+ 
+);
+
+
+ALTER TABLE accident ADD CONSTRAINT PK_accident PRIMARY KEY (accident_id);
+ALTER TABLE country ADD CONSTRAINT PK_countryPRIMARY KEY (country_id);
+ALTER TABLE description ADD CONSTRAINT PK_description PRIMARY KEY (description_id);
+
+ALTER TABLE country ADD CONSTRAINT FK_country_id FOREIGN KEY (country_id) REFERENCES accident (country_id);
+ALTER TABLE description ADD CONSTRAINT FK_description_id FOREIGN KEY (description_id) REFERENCES accident (description_id);
