dc066592cf22a482eaa15aad279993c128d0716a
LadaKondrashova <55762041+Lada678@users.noreply.github.com>
2023-01-26 08:58:05 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,19 @@
 --кількість смертей по країнам 
 
-SELECT country_id,  SUM(accident_death) 
-as sum_death FROM   accident GROUP BY country_id
+SELECT a.country_id, c.country_name, SUM(accident_death) as sum_death
+FROM accident a
+	JOIN country c ON a.country_id = c.country_id
+GROUP BY a.country_id, c.country_name
 
---
+--кількість випадків по кожній з причин 
+SELECT a.description_id, c.description_name, count(a.description_id) as amound
+FROM accident a
+	JOIN description c ON a.description_id = c.description_id
+GROUP BY a.description_id, c.description_name
+
+
+
+--кількість смертей по рокам
+SELECT  EXTRACT( year from accident_date), sum(accident_death) as deathrate 
+FROM accident 
+GROUP BY EXTRACT( year from accident_date)
